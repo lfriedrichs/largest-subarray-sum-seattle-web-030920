97ee7c829d108b127c02e4cf7767e176aefb2cd2
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-09 04:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 function largestSubarraySum(array) {
-  let sums = {};
-  for (let a=0; a<array.length-1; a++) {
-    if (array[a] > 0) {
-      sums[a] ={}
-      for (let z=a+1; z<array.length; z++) {
-        sums[a][z] = 0;
-        for (let i=a; i<=z; i++) {
-          sums[a][z] += array[i];
-        }
+  let sequence = 0
+  let largest = 0
+
+  for (let i = 0; i < array.length; i++) {
+    sequence = array[i]
+    if (sequence > largest) {
+      largest = sequence
+    }
+    for (let l = i + 1; l < array.length; l++) {
+      sequence = array[l] + sequence
+      if (sequence > largest) {
+        largest = sequence
       }
     }
   }
-  // console.log(sums)
-  let sumsToArr = [];
-  for (let key in sums) {
-    for (let k2 in sums[key])
-       sumsToArr.push(sums[key][k2]);
-  }
-  return sumsToArr.reduce((acc, cur) => Math.max(acc, cur));
+  
+  return largest
+
 }
