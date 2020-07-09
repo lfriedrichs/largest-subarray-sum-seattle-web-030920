f1fb7b1f23d5ff4c006c21ca437eb2246954263e
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-09 04:24:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-function largestSubarraySum(arr) {
-  let globalLargest = 0;
-  let localLargest = arr[0];
-  const subArray = arr.slice(1, arr.length - 1);
-
-  subArray.forEach(n => {
-    localLargest += n;
-    if (localLargest > globalLargest) { globalLargest = localLargest };
-  });
-
-  return globalLargest
+function largestSubarraySum(array) {
+  let sums = {};
+  for (let a=0; a<array.length-1; a++) {
+    if (array[a] > 0) {
+      sums[a] ={}
+      for (let z=a+1; z<array.length; z++) {
+        sums[a][z] = 0;
+        for (let i=a; i<=z; i++) {
+          sums[a][z] += array[i];
+        }
+      }
+    }
+  }
+  // console.log(sums)
+  let sumsToArr = [];
+  for (let key in sums) {
+    for (let k2 in sums[key])
+       sumsToArr.push(sums[key][k2]);
+  }
+  return sumsToArr.reduce((acc, cur) => Math.max(acc, cur));
 }
